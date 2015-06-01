e8406d639ebb9f8181e1b78744e71ff5f52aac9b
Ioannis Drositis <jdros_el@yahoo.com>
2015-06-01 23:32:00 +0100

    Add chef/opscode pkg key

--- vms/bootstrap_kitchen.sh
+++ vms/bootstrap_kitchen.sh
@@ -17,8 +17,7 @@ rpm -qi docker-io &>/dev/null || {
 # Install ChefDK 
 rpm -qi chefdk &>/dev/null || {
   echo "Setting up ChefDK ..."
-  #gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
-  gpg --keyserver keys.gnupg.net --recv-keys 83ef826a
+  rpm --import https://downloads.getchef.com/chef.gpg.key
   rpm -ihv https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.6.0-1.el6.x86_64.rpm
 }
 
@@ -28,5 +27,7 @@ sudo chef-apply /vagrant/recipe_chef_user.rb
 # Enable full-sudo for chef user; needed for docker
 sudo chef-apply /vagrant/recipe_chef_sudo.rb
 
-# Echo IP
-ip -4 a s en1
+# Echo IP(s)
+for i in 0 1; do
+  echo "IP/eth$i: "$(ip -4 a s eth$i|grep inet|awk '{print $2}'|cut -d'/' -f1)
+done
